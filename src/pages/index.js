f1dc1ba346596ba8c90bf24644962a8a01bb2950
Ian Lumagod <ianlumagod@gmail.com>
2022-07-29 22:42:46 +0800

    removed consent

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,6 @@ import { Helmet } from 'react-helmet'
 const IndexPage = () => {
   return (
     <div>
-      <Helmet>
-        <script
-          async="async"
-          src="https://eu-consent-icon-dev-sf.truste-svc.net/notice?domain=weather.com&locale=en&js=nj&noticeType=bb&country=gb&text=true&gtm=1"
-          crossOrigin=""
-          data-react-helmet="true"
-        />
-      </Helmet>
-      <div id="teconsent"></div>
-      <div id="consent_blackbar"></div>
       <title>Home Page</title>
       <h1>Welcome to my Gatsby site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>

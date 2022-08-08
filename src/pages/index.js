be7bb7b7dd4b268f95cb133172e4c505ef85f487
Ian Lumagod <ianlumagod@gmail.com>
2022-08-08 13:07:34 +0800

    used React Helment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,30 @@
 // Step 1: Import React
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
 // import Footer from "../components/footer";
 
 // Step 2: Define your component
 const IndexPage = () => {
-  useEffect(() => {
-    const taScript = document.createElement("script");
-    taScript.src = "https://eu-consent-icon-dev-sf.truste-svc.net/notice?domain=weather.com&locale=en&js=nj&noticeType=bb&country=gb&text=true&gtm=1";
-    document.head.appendChild(taScript);
+  // useEffect(() => {
+  //   const taScript = document.createElement("script");
+  //   taScript.src = "https://eu-consent-icon-dev-sf.truste-svc.net/notice?domain=weather.com&locale=en&js=nj&noticeType=bb&country=gb&text=true&gtm=1";
+  //   document.head.appendChild(taScript);
 
-    return () => {
-      taScript.remove();
-    }
-  }, []);
-
+  //   return () => {
+  //     taScript.remove();
+  //   }
+  // }, []);
 
   return (
     <div>
+      <Helmet>
+        <script
+          async="async"
+          src="https://eu-consent-icon-dev-sf.truste-svc.net/notice?domain=weather.com&locale=en&js=nj&noticeType=bb&country=gb&text=true&gtm=1"
+          crossOrigin=""
+          data-react-helmet="true"
+        />
+      </Helmet>
       <div>GATSBY TEST PAGE</div>
       <div>
         <div id="teconsent"></div>
